0963cde0226e874fec8c8ef871f01a71787c0768
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-07-22 22:57:28 -0700

    All the http methods with error response

--- POC of DAO and HTTP Basic Authentication/StarbucksTeamApp/src/main/java/com/uncharted/CustomerResource.java
+++ POC of DAO and HTTP Basic Authentication/StarbucksTeamApp/src/main/java/com/uncharted/CustomerResource.java
@@ -14,22 +14,18 @@ public class CustomerResource {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getIt(@PathParam("id") int id, @HeaderParam("authorization") String authString) {
-        //JSONObject jsonObj = null;
         String jsonString=null;
         try {
             if (!AuthenticationClass.bCheckIfAuthenticated(authString)) {
                 String json = "{error:User not authenticated}";
 
-                Response.status(401).build();
+              return Response.status(401).build();
             }
             try {
 
                 ObjectMapper mapper = new ObjectMapper();
                 jsonString= mapper.writeValueAsString(AuthenticationClass.customerBO);
                 System.out.println("jsonString:" + jsonString);
-
-
-               // jsonObj = new JSON(jsonString);
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -43,4 +39,56 @@ public class CustomerResource {
         }
         return Response.ok(jsonString).build();
     }
+
+    @POST
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insertCustomer(@PathParam("id") int id, @HeaderParam("authorization") String authString)
+    {
+
+        try
+        {
+
+
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+      return Response.status(200).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateCustomer(@PathParam("id") int id, @HeaderParam("authorization") String authString)
+    {
+
+        try
+        {
+
+
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return Response.status(200).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCustomer(@PathParam("id") int id, @HeaderParam("authorization") String authString)
+    {
+
+        try
+        {
+
+
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return Response.status(200).build();
+    }
+
 }
